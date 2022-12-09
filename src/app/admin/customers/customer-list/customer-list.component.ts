6c536435c28773e9838b38bbed7eb64051d30c8b
tharindu_madushan <htharindu971@gmail.com>
2022-12-09 05:57:13 +0530

    add-customer-list

--- src/app/admin/customers/customer-list/customer-list.component.ts
+++ src/app/admin/customers/customer-list/customer-list.component.ts
@@ -1,10 +1,34 @@
-import { Component } from '@angular/core';
+import { Component, OnDestroy, OnInit } from '@angular/core';
+import { Subject } from 'rxjs';
+import { Customer } from 'src/app/models/customer.model';
+import { CustomerStore } from 'src/app/services/customer/customer.store';
 
 @Component({
   selector: 'app-customer-list',
   templateUrl: './customer-list.component.html',
   styleUrls: ['./customer-list.component.css']
 })
-export class CustomerListComponent {
+export class CustomerListComponent implements OnInit,OnDestroy{
+  data:Customer[]=[]
+  dtOptions: DataTables.Settings = {};
+  dtTrigger: Subject<any> = new Subject<any>();
+  
+  ngOnInit(): void {
+    this.getData();
+  }
+  
+  
 
+  constructor(private customerStore:CustomerStore){}
+
+  getData(){
+    this.customerStore.customers$.subscribe(data=>{
+      this.data = data
+      console.log(this.data)
+      this.dtTrigger.next(data);
+    })
+  }
+  ngOnDestroy(): void {
+    this.dtTrigger.unsubscribe();
+  }
 }
